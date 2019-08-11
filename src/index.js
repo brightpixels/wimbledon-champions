f7e2254d2cc4a3db12afdcf4b6dec48a5f030bc7
Abdi Saeed <abdi.saeed2009@gmail.com>
2019-08-11 14:15:49 +0100

    Update module API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 import data from "./data.json";
-
-export const all = data;
-export const ladies = data.map(({ gentlemen, ...rest }) => rest);
-export const gentlemen = data.map(({ ladies, ...rest }) => rest);
-export const year = year => x.find(i => i.year.toString() === year);
+export default () => data;
